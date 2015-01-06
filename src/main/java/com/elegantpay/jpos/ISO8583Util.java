db85adb597f1fa23836f0b269ac7652471e868d7
yinheli <me@yinheli.com>
2015-01-06 17:17:55 +0800

    add comments, header decode, and remove sysout

--- src/main/java/com/elegantpay/jpos/ISO8583Util.java
+++ src/main/java/com/elegantpay/jpos/ISO8583Util.java
@@ -5,6 +5,8 @@ import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISOUtil;
 import org.jpos.iso.packager.GenericPackager;
 
+import java.util.Arrays;
+
 /**
  * @author yinheli
  */
@@ -23,16 +25,31 @@ public class ISO8583Util {
     private ISO8583Util() {
     }
 
+    /**
+     * 从原始报文(hex)解析为 ISOMsg 对象
+     *
+     * @param hex 16进制数据 包含长度(2字节)+11字节的标准 header
+     * @return
+     * @throws Exception
+     */
     public static ISOMsg unpack(String hex) throws Exception {
-        hex = hex.replaceAll("\\s", "").substring(26);
+        hex = hex.replaceAll("\\s", "");
         return unpack(ISOUtil.hex2byte(hex));
     }
 
+    /**
+     * 从原始报文(byte[])解析为 ISOMsg 对象
+     *
+     * @param raw 原始报文, 包含长度(2字节)+11字节的标准 header
+     * @return
+     * @throws Exception
+     */
     public static ISOMsg unpack(byte[] raw) throws Exception {
-        System.out.println(ISOUtil.hexdump(raw));
+        byte[] header = Arrays.copyOfRange(raw, 2, 13);
         ISOMsg msg = new ISOMsg();
         msg.setPackager(packager);
-        msg.unpack(raw);
+        msg.unpack(Arrays.copyOfRange(raw, 13, raw.length));
+        msg.setHeader(header);
         return msg;
     }
 
